88bd425ca0bc99ef40a3302986812150791ac694
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-05-08 10:42:21 +0300

    Symptomps

---
+++ components/symptomps.vue
@@ -0,0 +1,25 @@
+<template
+  ><div class="symptomps">
+    <div class="covid">
+      <h3>Covid-19</h3>
+      <h2>Symptomps</h2>
+      <p>
+        Corona viruses are a type of virus. There are many different kinds, and
+        some cause disease. A newly identified type has caused a recent outbreak
+        of respiratory
+      </p>
+    </div>
+    <img src="@/static/Group 15444.svg" alt="symptomps" /></div
+></template>
+<style lang="scss" scoped>
+.covid {
+  margin-top: 100px;
+  width: 50%;
+}
+.symptomps {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,16 @@
     <app-header />
     <app-coronovirus />
     <app-contagion />
+    <app-symptoms />
   </div>
 </template>
 <script>
 import appHeader from "@/components/header";
 import appContagion from "@/components/contagion";
 import appCoronovirus from "@/components/coronavirus";
+import appSymptoms from "@/components/symptomps";
 export default {
-  components: { appCoronovirus, appHeader, appContagion },
+  components: { appCoronovirus, appHeader, appContagion, appSymptoms },
   head() {
     return {
       title: "Covid 19"
